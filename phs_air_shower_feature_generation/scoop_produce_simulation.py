08b76f29492a4b0d836b3591727f36eaf2a99a85
Sebastian Mueller <sebmuell@phys.ethz.ch>
2017-09-27 18:29:47 +0200

    get scoop running

--- phs_air_shower_feature_generation/scoop_produce_simulation.py
+++ phs_air_shower_feature_generation/scoop_produce_simulation.py
@@ -1,11 +1,11 @@
 """
 Call with 'python -m scoop --hostfile scoop_hosts.txt'
 
-Usage: phs.extract.air-shower.features.sim --sim_dir=DIR --out_dir=DIR
+Usage: phs.extract.air-shower.features.sim --sim_block_dir=DIR --out_dir=DIR
 
 Options:
-    --sim_dir=DIR   The input phs/sim/ directory with the '.phs.jsonl' runs
-    --out_dir=DIR   The output directory
+    --sim_block_dir=DIR   The input phs/sim/block directory (e.g. gamma_gustav/ ) with the '.phs.jsonl' runs
+    --out_dir=DIR         The output directory
 """
 import docopt
 import scoop
@@ -21,25 +21,22 @@ import phs_air_shower_feature_generation as psfg
 
 def extract_features(job): 
     makedirs(dirname(job['feature_path']), exist_ok=True)
-    try:
-        psfg.extract_from_simulation(
-            path=job['input_path'], 
-            out_path=job['feature_path']
-        )
-    except:
-        return 0
+    psfg.extract_from_simulation(
+        path=job['input_path'], 
+        out_path=job['feature_path']
+    )
     return 1
 
 
 def main():
     try:
         arguments = docopt.docopt(__doc__)
-        sim_dir = arguments['--sim_dir']
+        sim_block_dir = arguments['--sim_block_dir']
         out_dir = arguments['--out_dir']
 
         jobs = []
-        for input_path in glob(join(obs_dir,'*','*.phs.jsonl.gz')):
-            feature_path = join(out_dir, basename(input_path).split('.')[0]+'.sim.features.msg')
+        for input_path in glob(join(sim_block_dir,'*.phs.jsonl.gz')):
+            feature_path = join(out_dir, basename(input_path).split('.')[0]+'.features.msg')
             if not exists(feature_path):
                 jobs.append({'input_path': input_path, 'feature_path': feature_path})
 
