f9a04e1ee8ee1e1fd61b982685c022394eb5dcdb
sebastian <sebmuell@phys.ethz.ch>
2017-09-29 14:58:04 +0200

    read in many DataFrames

--- photon_stream_analysis/tools.py
+++ photon_stream_analysis/tools.py
@@ -5,3 +5,11 @@ def cartesian2polar(x, y):
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
     return(rho, phi)
+
+
+def read_and_concat_DataFrames(wild_card='*.msg'):
+    dataframes = []
+    for input_path in glob(wild_card):
+        dataframes.append(reduce_to_32_bit(pd.read_msgpack(input_path)))
+    return pd.concat(dataframes)
+
