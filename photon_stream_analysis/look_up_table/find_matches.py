bfc18110083905df95fc6a1d5056cfdadb27cd97
sebastian <sebmuell@phys.ethz.ch>
2017-10-10 22:26:26 +0200

    first match catcher

--- photon_stream_analysis/look_up_table/find_matches.py
+++ photon_stream_analysis/look_up_table/find_matches.py
@@ -1,4 +1,25 @@
 import numpy as np
+import photon_stream as ps
+from .produce import rrr
+from .distance_metric import difference_image
+
+
+def find_matches(event_features, lut, cog_angular_distance=np.deg2rad(0.175)):
+    evt = event_features
+
+    ul = evt['number_photons']*1.05
+    ll = evt['number_photons']*0.95
+    valid_num_ph = lut.idx_for_number_photons_within(ll, ul)
+
+    ul = evt['cog_cx_pap'] + cog_angular_distance
+    ll = evt['cog_cx_pap'] - cog_angular_distance
+    valid_cog_cx_pap = lut.idx_for_cog_cx_pap_within(ll, ul)
+
+    ul = evt['cog_cy_pap'] + cog_angular_distance
+    ll = evt['cog_cy_pap'] - cog_angular_distance
+    valid_cog_cy_pap = lut.idx_for_cog_cy_pap_within(ll, ul)
+
+    return not_unique([valid_num_ph, valid_cog_cx_pap, valid_cog_cy_pap])
 
 
 def not_unique(list_of_index_arrays):
@@ -6,3 +27,18 @@ def not_unique(list_of_index_arrays):
     idxs = np.concatenate(list_of_index_arrays)
     u = np.unique(idxs, return_counts=True)
     return u[0][u[1] == l]
+
+
+def match(event, lut):
+    event_features = rrr(event)
+    match_candidates = find_matches(event_features, lut)
+
+    image = ps.representations.raw_phs_to_image(event.photon_stream.raw)
+
+    for candidate in match_candidates:
+        ccandidate_image = lut.image(candidate)
+        image_diff = difference_image(image, ccandidate_image)
+        print(image_diff)
+
+
+
